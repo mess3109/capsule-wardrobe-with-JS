32e06f43d5aec4194027634fa3e48400536bad57
Mollie <mollie.stein@gmail.com>
2017-08-26 17:32:40 -0400

    added nested routes for outfits

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 
   resources :seasons, :only => [:index, :show]
   resources :categories, :only => [:index, :show]
-  
+
   resources :items
-  resources :outfits
-  
-  root 'static#index', as: 'index'
+
+  resources :outfits do
+    resources :items, only: [:show, :index]
+  end
+
+  root 'static#index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
