eb9c9515b02706aec8ef5aef963ac0ca3e6b4b19
Mollie <mollie.stein@gmail.com>
2017-08-25 22:36:37 -0400

    added resources for category, item, outfit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:github]
 
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, length: {is: 8}, allow_blank: false
+
  def self.from_omniauth(auth)  
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :items
+  resources :outfits
+  resources :categories, :only => [:show]
+  
   root 'static#index', as: 'index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
