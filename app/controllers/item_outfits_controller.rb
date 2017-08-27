447831d43e0632c2d4910eb8d2e02d463a817748
Mollie <mollie.stein@gmail.com>
2017-08-27 15:01:59 -0400

    removed unnecessary controllers and can remote item from outfit

--- app/controllers/item_outfits_controller.rb
+++ app/controllers/item_outfits_controller.rb
@@ -6,4 +6,10 @@ class ItemOutfitsController < ApplicationController
 	    item_outfit.save
 	    redirect_to outfit_path(current_user.current_outfit)
 	end
+
+	def destroy
+		item_outfit = ItemOutfit.find_by(:item_id => params[:item], :outfit_id => params[:outfit])
+		item_outfit.delete
+		redirect_to outfit_path(Outfit.find(params[:outfit]))
+	end
 end
