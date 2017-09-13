5f896740b835a04946d8bd5725dd9599237c88f3
Mollie <mollie.stein@gmail.com>
2017-09-12 20:34:16 -0400

    created items_not_in_outfit outfit helper

--- app/helpers/outfits_helper.rb
+++ app/helpers/outfits_helper.rb
@@ -3,4 +3,9 @@ module OutfitsHelper
 	def has_items?(outfit)
 		!outfit.items.empty?
 	end
+
+	def items_not_in_outfit(outfit)
+		(current_user.items - outfit.items).sort_by { |item| item.category.title }
+	end
+
 end
