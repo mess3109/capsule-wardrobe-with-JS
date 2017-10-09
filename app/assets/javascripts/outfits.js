9865e3f8ca64ad53f26e577b5ce7844ff5bfe47f
Mollie <mollie.stein@gmail.com>
2017-10-09 14:47:54 -0400

    removed turbolinks and .coffee files

--- app/assets/javascripts/outfits.js
+++ app/assets/javascripts/outfits.js
@@ -1,9 +1,10 @@
 $(function () {
-  $(".js-next").on("click", function() {
-    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-    $.get("/outfits/" + nextId + ".json", function(data) {
-      $(".title").text(data["title"]);
-      $(".js-next").attr("data-id", data["id"]);
-    });
-  });
-});
+	$(".js-next").on("click", function() {
+		var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+		$.get("/outfits/" + nextId + ".json", function(data) {
+			$(".title").text(data["title"]);
+			$(".season").text(data["season"]["title"]);
+			$(".js-next").attr("data-id", data["id"]);
+		});
+	});
+}); 
