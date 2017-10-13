dbad21da7ec0455ca0cfb9517874b92154645d21
Mollie <mollie.stein@gmail.com>
2017-10-12 21:12:52 -0400

    added respond_to html or json to destroy and create actions in item_outfits controller

--- app/controllers/item_outfits_controller.rb
+++ app/controllers/item_outfits_controller.rb
@@ -3,13 +3,19 @@ class ItemOutfitsController < ApplicationController
 	def create
 		current_user.current_outfit = Outfit.find(params[:outfit_id])
 		@item_outfit = current_user.current_outfit.add_item(params[:item_id])
-		render json: @item_outfit, :include => [:item, :outfit]
-
+		respond_to do |format|
+			format.html { redirect_to outfit_path(current_user.current_outfit) }
+			format.json { render json: @item_outfit, :include => [:item, :outfit] }
+		end
 	end
 
 	def destroy
 		item_outfit = ItemOutfit.find_by(:item_id => params[:item], :outfit_id => params[:outfit])
 		item_outfit.delete
 		render body: nil, status: :no_content
+		respond_to do |format|
+			format.html { redirect_to outfit_path(Outfit.find(params[:outfit])) }
+			format.json { render body: nil, status: :no_content }
+		end
 	end
 end
