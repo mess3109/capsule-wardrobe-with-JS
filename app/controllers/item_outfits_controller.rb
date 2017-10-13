f0531cd45a914aff863f8f769e5e433b1f89b537
Mollie <mollie.stein@gmail.com>
2017-10-13 00:27:37 -0400

    item_outfits controller only responds to json

--- app/controllers/item_outfits_controller.rb
+++ app/controllers/item_outfits_controller.rb
@@ -3,19 +3,12 @@ class ItemOutfitsController < ApplicationController
 	def create
 		current_user.current_outfit = Outfit.find(params[:outfit_id])
 		@item_outfit = current_user.current_outfit.add_item(params[:item_id])
-		respond_to do |format|
-			format.html { redirect_to outfit_path(current_user.current_outfit) }
-			format.json { render json: @item_outfit, :include => [:item, :outfit] }
-		end
+		render json: @item_outfit, :include => [:item, :outfit]
 	end
 
 	def destroy
 		item_outfit = ItemOutfit.find_by(:item_id => params[:item], :outfit_id => params[:outfit])
 		item_outfit.delete
 		render body: nil, status: :no_content
-		respond_to do |format|
-			format.html { redirect_to outfit_path(Outfit.find(params[:outfit])) }
-			format.json { render body: nil, status: :no_content }
-		end
 	end
 end
