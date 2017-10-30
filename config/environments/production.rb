05ce745c6a0b15c90cdea4ef027f5567af039e7c
Mollie <mollie.stein@gmail.com>
2017-10-30 19:46:07 -0400

    fixing syntax error in production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   storage: :s3,
-  region: ENV['AWS_REGION']
+  s3_region: ENV['AWS_REGION'],
   s3_credentials: {
     bucket: ENV['AWS_S3_BUCKET'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
