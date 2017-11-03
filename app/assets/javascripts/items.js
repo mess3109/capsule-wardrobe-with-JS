3bb77d1c5ee2697fd4ae0a56df8c4b4b924d4f34
Mollie <mollie.stein@gmail.com>
2017-11-03 15:44:59 -0400

    adding more to item preview

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -6,8 +6,11 @@ $(document).ready(function () {
 			let id = $(this).attr("data-id")
 			$.get(`/items/${id}.json`)
 			.done(function(item) {
+				console.log(item.image)
 				$('.item-show-spec').empty()
 				$('.item-show-spec').append(`<h4>${item.title}</h4>`)
+				$('.item-show-spec').append(`<br><img src=${item.image} alt="" width='40' height='40'/>`)
+				$('.item-show-spec').append(`<br>Category: ${item.category.title}`)
 				$('.item-show-spec').append(`<br>Color: ${item.color}`)
 				$('.item-show-spec').append(`<br>Brand: ${item.brand}`)
 				$('.item-show-spec').append(`<br># of Outfits: ${item.outfits.length}`)
@@ -26,10 +29,10 @@ function Item(attributes) {
 Item.prototype.RemoveClothingItemLink = function(outfit_id) {
 	return (`
 		<li id="item-${this.id}">
-			<a href="/outfits/${outfit_id}/items/${this.id}">${this.title}</a> - ${this.category.title}
-			<a rel="nofollow" class="delete-url" data-id="${this.id}" data-method="delete" href="/item_outfits/${outfit_id}?item=${this.id}&amp;outfit=${outfit_id}">Remove</a>
+		<a href="/outfits/${outfit_id}/items/${this.id}">${this.title}</a> - ${this.category.title}
+		<a rel="nofollow" class="delete-url" data-id="${this.id}" data-method="delete" href="/item_outfits/${outfit_id}?item=${this.id}&amp;outfit=${outfit_id}">Remove</a>
 		</li>
-	`)
+		`)
 }
 
 Item.prototype.AddClothingItemLink = function(outfit_id) {
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,6 @@
 class ItemSerializer < ActiveModel::Serializer
   # default_includes :category, :outfits
-  attributes :id, :title, :color, :condition, :brand, :category
+  attributes :id, :title, :color, :condition, :brand, :category, :image
   has_many :outfits
  
 end
